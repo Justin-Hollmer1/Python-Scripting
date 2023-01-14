bd7a3c6a85cbb985775b56fbf939e1444872710a
Justin Hollmer <justin.hollmer@gmail.com>
2023-01-14 17:37:20 -0600

    it's now able to read the phone number off of resumes

--- phone_and_email_scraper.py
+++ phone_and_email_scraper.py
@@ -12,8 +12,22 @@ pdfFile = open('../Justin_Hollmer_Resume.pdf', 'rb')
 pdfReader = PyPDF2.PdfReader(pdfFile)
 
 # A quick test to make sure that it al works correctly
-print("The resume has: " + str(len(pdfReader.pages)) + " pages")
+# print("The resume has: " + str(len(pdfReader.pages)) + " pages")
+
+# Now storing the page text in the page text variable.
 pageObject = pdfReader.pages[0]
 pageText = pageObject.extract_text()
-print("The page text is " + pageText)
+# print("The page text is " + pageText)
 
+# Defining a couple of other parameters
+phoneNumberRegex = re.compile(r'\(\d\d\d\) \d\d\d-\d\d\d\d')
+otherPhoneNumberRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneNumber = phoneNumberRegex.search(pageText)
+
+
+
+
+
+
+
+print(phoneNumber.group())
